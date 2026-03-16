5727d7a80e121975c0d0332bfdf9b736f92756e1
allisson012 <allissoncastilho@gmail.com>
2026-03-16 15:50:58 -0300

    criando form request e ,melhorando as mensagens de erro

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -13,11 +14,9 @@ class LoginController extends Controller
         return view('login');
     }
 
-    public function authenticate(Request $request){
-        $credentials = $request->validate([
-            'email' => 'required|email',
-            'password' => 'required|min:6',
-        ]);
+    public function authenticate(LoginRequest $request){
+        
+        $credentials = $request->only('email','password');
 
         if(Auth::attempt($credentials)){
             $request->session()->regenerate();
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,35 +1,53 @@
 <x-layout>
     <main class="py-10">
-        <h1>
-            Faça o login
-        </h1>
-        <section class="mt-4">
-            <form action=" {{ route('auth.login') }}" method="POST">
-                @csrf
-                @error('email')
-                    <p class="text-red-500 text-xl mt-1"> 
-                        {{ $message }} 
+        <section class="bg-white max-w-[600px] nx-auto p-10 border-2 mt-4">
+
+                    <h1 class="font-bold text-3xl  ">
+                        Faça login
+                    </h1>
+                    <p>
+                        Insira seus dados para acessar
                     </p>
-                @enderror
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="your@email.com"
-                  class="bg-white p-2 border-2"
-                >
-                <input
-                  type="password"
-                  name="password"
-                  placeholder="*******"
-                  class="bg-white p-2 border-2"
-                >
-                <button 
-                type="submit"
-                class="bg-white p-2 border-2"
-                >
-                  Entrar
-                </button>
-            </form>
+                    <form action=" {{ route('auth.login') }}" method="POST" class="flex flex-col">
+                    @csrf
+                        <div class="flex flex-col gap-2 mb-2">
+                            <label for="email">Email</label>
+                            <input
+                            type="email"
+                            name="email"
+                            placeholder="your@email.com"
+                            class="bg-white p-2 border-2 @error('email') border-red-500 @enderror"
+                            >
+
+                        @error('email')
+                        <p class="text-red-500 text-sm">{{ $message }}</p>
+                        @enderror
+
+                        </div>
+                        <div class="flex flex-col gap-2 mb-2">
+                            <label for="password">
+                                Senha
+                            </label>
+                            <input
+                            type="password"
+                            name="password"
+                            placeholder="*******"
+                            class="bg-white p-2 border-2 @error('password') border-red-500 @enderror"
+                            >
+
+                            @error('password')
+                                <p class="text-red-500 text-sm">{{ $message }}</p>
+                            @enderror
+                            
+                        </div>
+
+                        <button 
+                        type="submit"
+                        class="bg-white p-2 border-2"
+                        >
+                        Entrar
+                        </button>
+                    </form>
         </section>
     </main>
 </x-layout>
