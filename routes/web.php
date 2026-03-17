0a852face36ec182fcb730b33425b168d60d9978
allisson012 <allissoncastilho@gmail.com>
2026-03-17 15:55:46 -0300

    Criando view/controller/request para register

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SiteController;
 use App\Http\Controllers\Auth\LoginController;
-
+use App\Http\Controllers\RegisterController;
 Route::get('/', [SiteController::class, 'index'])->name('site.index');
 
 Route::get('/login', [LoginController::class, 'index'])->name('site.login');
 
 Route::post('/login',[LoginController::class, 'authenticate'])->name('auth.login');
+
+Route::get('/cadastro', [RegisterController::class, 'index'])->name('site.register');
+
+Route::post('/cadastro', [RegisterController::class, 'store'])->name('auth.register');
 
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [SiteController::class, 'dashboard'])->name('site.dashboard');
